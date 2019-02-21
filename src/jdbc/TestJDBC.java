0a475c55e40fc7724a9493d75fbafbe66b687d67
supmatt <sechenxi@hotmail.com>
2019-02-21 15:14:36 -0500

    update java database

--- src/jdbc/TestJDBC.java
+++ src/jdbc/TestJDBC.java
@@ -2,6 +2,7 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestJDBC {
 	public static void main(String[] args) {
@@ -11,7 +12,15 @@ public class TestJDBC {
 			Connection c = DriverManager
 					.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8", 
 							"root", "admin");
-			System.out.println("connect but and get the but is " + c);
+//			System.out.println("connect but and get the but is " + c);
+			
+			Statement s = c.createStatement();
+//			System.out.println("Get statement taget: " + s);
+			String sql = "insert into hero values(null,"+"'tmo'"+","+333.0f+"," +50+")";
+			s.execute(sql);
+			System.out.println("success insert tmo information");
+			
+			
 		} catch (ClassNotFoundException e) {
 			// TODO: handle exception
 			e.printStackTrace();
