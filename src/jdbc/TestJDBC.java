a9081fd1e852bc1bc4d2fb761edf7594d49366cd
supmatt <sechenxi@hotmail.com>
2019-02-24 19:59:22 -0500

    Tomcat一直没有解决  重新启动在实验一下

--- src/jdbc/TestJDBC.java
+++ src/jdbc/TestJDBC.java
@@ -6,50 +6,87 @@ import java.sql.Statement;
 
 public class TestJDBC {
 	public static void main(String[] args) {
-		Connection c = null;
-		Statement s = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
+//		Connection c = null;
+//		Statement s = null;
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
 //			System.out.println("Database driver success");
-			 c = DriverManager
-					.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8", 
-							"root", "admin");
+//			 c = DriverManager
+//					.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8", 
+//							"root", "admin");
 //			System.out.println("connect but and get the but is " + c);
 			
-			 s = c.createStatement();
+//			 s = c.createStatement();
 //			System.out.println("Get statement taget: " + s);
-			 for(int i = 1; i<3; i++) {
-				 String ii = String.valueOf(i);
+//			 for(int i = 1; i<3; i++) {
+//				 String ii = String.valueOf(i);
 //				 String sql = "insert into hero values(null,"+"'Hero'" +","+333.0f+"," +50+")";
-				 String iname = "name" +ii;
-				 String sql = "insert into hero values(null," + "'"+iname+"'" +" ,+333.0 ,50)";
-					s.execute(sql);
-			 }
+//				 String iname = "name" +ii;
+//				 String sql = "insert into hero values(null," + "'"+iname+"'" +" ,+333.0 ,50)";
+//					s.execute(sql);
+//			 }
 //			String sql = "insert into hero values(null,"+"'tmoo'"+","+333.0f+",
 //			 " +50+")";
 //			 String sql = "insert into hero values(null, 'tmoo00' ,+333.0 ,50)";
 //			s.execute(sql);
-			System.out.println("success insert tmo information");
-			
-			
-		} catch (ClassNotFoundException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if(s !=null)
-				try {s.close();
-				}catch(SQLException e) {
-					e.printStackTrace();
-				}
-			if(c != null)
-				try {
-					c.close();
-				} catch (Exception e) {
-					// TODO: handle exception
-					e.printStackTrace();
-				}
-		}
+//			System.out.println("success insert tmo information");
+//		} catch (ClassNotFoundException e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			if(s !=null)
+//				try {s.close();
+//				}catch(SQLException e) {
+//					e.printStackTrace();
+//				}
+//			if(c != null)
+//				try {
+//					c.close();
+//				} catch (Exception e) {
+//					// TODO: handle exception
+//					e.printStackTrace();
+//				}
+//		}
+		try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+//		try (
+//	            Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8",
+//	                "root", "admin");
+//	            Statement s = c.createStatement();             
+//	        )
+//		{
+////            String sql = "insert into hero values(null," + "'ÌáÄª'" + "," + 313.0f + "," + 50 + ")";
+////            String sql = "delete from hero where id = 23";
+//			String sql = "update hero set name = 'name3', hp = 340 where id = 21";
+//            
+//            s.execute(sql);
+//              
+//        } catch (SQLException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+		execute("insert into hero values(null," + "'ÌáÄª2'" + "," + 313.0f + "," + 50 + ")");
+		
+	}
+	public static void execute(String sql) {
+		try (
+	            Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8",
+	                "root", "admin");
+	            Statement s = c.createStatement();             
+	        )
+	        {
+	            String Sql = sql;
+	            s.execute(Sql);
+	               
+	        } catch (SQLException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
 	}
 }
+// Tomcat can not connect, next day continue
