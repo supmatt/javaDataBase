565da40ae6a1b961db502923beff9fb649d30fe6
supmatt <sechenxi@hotmail.com>
2019-03-01 16:01:16 -0500

    avantage of PreparedStatement

--- src/jdbc/TestJDBC.java
+++ src/jdbc/TestJDBC.java
@@ -1,6 +1,7 @@
 package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -93,8 +94,8 @@ public class TestJDBC {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-		list(6,3);
-	}
+//		list(6,3);
+//	}
 //		 try (Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8",
 //	                "root", "admin"); Statement s = c.createStatement();) {
 	 
@@ -120,25 +121,38 @@ public class TestJDBC {
 			 
 //			 String sql = "select count(*) from hero";
 //			 
-	public static void list(int start, int count) {
-		try (Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8",
-                "root", "admin"); Statement s = c.createStatement();) {
-			String sql = "select * from hero limit " + start +","+count+"";
-			//add word "limit for control output information
-			 
-			 ResultSet rs = s.executeQuery(sql);
-			 while (rs.next()) {
-	                int id = rs.getInt("id");// 可以使用字段名
-	                String name = rs.getString(2);// 也可以使用字段的顺序
-	                float hp = rs.getFloat("hp");
-	                int damage = rs.getInt(4);
-	                System.out.printf("%d\t%s\t%f\t%d%n", id, name, hp, damage);
-	            }
-			 
-	        } catch (SQLException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	        }
+//	public static void list(int start, int count) {
+//		try (Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8",
+//                "root", "admin"); Statement s = c.createStatement();) {
+//			String sql = "select * from hero limit " + start +","+count+"";
+//			//add word "limit for control output information
+//			 
+//			 ResultSet rs = s.executeQuery(sql);
+//			 while (rs.next()) {
+//	                int id = rs.getInt("id");// 可以使用字段名
+//	                String name = rs.getString(2);// 也可以使用字段的顺序
+//	                float hp = rs.getFloat("hp");
+//	                int damage = rs.getInt(4);
+//	                System.out.printf("%d\t%s\t%f\t%d%n", id, name, hp, damage);
+//	            }
+//			 
+//	        } catch (SQLException e) {
+//	            // TODO Auto-generated catch block
+//	            e.printStackTrace();
+//	        }
+		String sql = "insert into hero values(null,?,?,?)";
+		try (Connection c = DriverManager.getConnection(
+				"jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8","root", "admin");
+				PreparedStatement ps = c.prepareStatement(sql);)
+		{
+			ps.setString(1, "tt");
+			ps.setFloat(2, 313.0f);
+			ps.setInt(3, 52);
+			ps.execute();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 		
 	}
 		
